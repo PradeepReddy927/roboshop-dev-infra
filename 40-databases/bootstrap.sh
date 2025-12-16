fcc3988dbba8351d7648affd66b0183a8943f343
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-12-16 12:49:39 +0530

    Terraform roboshop

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 dnf install ansible -y
-ansible-pull -u https://github.com/PradeepReddy927/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/PradeepReddy927/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -17,7 +17,8 @@ resource "aws_instance" "mongodb" {
 resource "terraform_data" "mongodb" {
   triggers_replace = [
     aws_instance.mongodb.id
- ]
+  ]
+ 
   connection {
       type     = "ssh"
       user     = "ec2-user"
