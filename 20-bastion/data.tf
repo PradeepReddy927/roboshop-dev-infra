367d96b3229d5198f295dde280327d57c91004d0
pradeepreddy25 <prem.pradeepreddy19@gmail.com>
2026-02-02 10:55:14 +0530

    terraform-roboshop

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -1,13 +1,11 @@
-data  "aws_ami" "joindevops" {
-      owners           = ["973714476881"]
-      most_recent      = true
-
-
+data "aws_ami" "joindevops" {
+    owners           = ["973714476881"]
+    most_recent      = true
+    
     filter {
         name   = "name"
         values = ["RHEL-9-DevOps-Practice"]
-    }     
-
+    }
 
     filter {
         name   = "root-device-type"
@@ -21,9 +19,9 @@ data  "aws_ami" "joindevops" {
 }
 
 data "aws_ssm_parameter" "bastion_sg_id" {
-   name = "/${var.project_name}/${var.environment}/bastion_sg_id"
+  name = "/${var.project_name}/${var.environment}/bastion_sg_id"
 }
 
 data "aws_ssm_parameter" "public_subnet_ids" {
-   name  = "/${var.project_name}/${var.environment}/public_subnet_ids"
+  name = "/${var.project_name}/${var.environment}/public_subnet_ids"
 }
