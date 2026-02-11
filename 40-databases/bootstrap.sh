ab8c21079d9c4851e122cedc55b90a180f2842bc
pradeepreddy25 <prem.pradeepreddy19@gmail.com>
2026-02-11 12:58:48 +0530

    terraform roboshop

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -4,12 +4,12 @@
 component=$1
 environment=$2
 dnf install ansible -y
-#ansible-pull -U https://github.com/daws-86s/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+#ansible-pull -U https://github.com/PradeepReddy927/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 # git clone ansible-playbook
 # cd ansible-playbook
 # ansible-playbook -i inventory main.yaml
 
-REPO_URL=https://github.com/daws-86s/ansible-roboshop-roles-tf.git
+REPO_URL=https://github.com/PradeepReddy927/ansible-roboshop-roles-tf.git
 REPO_DIR=/opt/roboshop/ansible
 ANSIBLE_DIR=ansible-roboshop-roles-tf
 
