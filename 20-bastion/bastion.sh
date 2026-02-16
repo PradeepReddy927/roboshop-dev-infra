5e33d0b5ffb75fbc3e22f8e9356ec51bba73ea7d
pradeepreddy25 <prem.pradeepreddy19@gmail.com>
2026-02-16 16:52:37 +0530

    terraform roboshop

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -4,3 +4,12 @@
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
+
+
+# creating databases
+cd /home/ec2-user
+git clone https://github.com/PradeepReddy927/roboshop-dev-infra.git
+chown ec2-user:ec2-user -R roboshop-dev-infra
+cd roboshop-dev-infra/40-databases
+terraform init
+terraform apply -auto-approve
