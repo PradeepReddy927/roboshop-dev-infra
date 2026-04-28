04cafccd75a6651da80027d0af7e2e6c9011d88e
pradeepreddy25 <prem.pradeepreddy19@gmail.com>
2026-04-28 11:11:33 +0530

    terraform roboshop

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+   default = "dawsdevops86.fun"
+}
+
+variable "zone_id" {
+    default = "Z0883062RHMIRSI7AY3N"
+}
